c8e8c70ce0357896621be60749ef92f32432a91b
Boniface <cheruiyotbony@gmail.com>
2024-01-21 16:12:10 +0300

    done

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,47 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    
+    def __init__(self,name='Boniface',job='Purchasing'):
+        self.name = name
+        self.job = job
+        
+        
+    def get_name(self):
+        return self._name
+    
+    def set_name(self,name):
+    
+        if type(name) is str and 1 <= len(name) < 25:
+            self._name = name.title()
+        else:
+            print('Name must be string between 1 and 25 characters.')
+            
+    name=property(get_name,set_name)
+    
+    
+    
+    def get_job(self):
+        return self._job
+
+
+    def set_job(self,job):
+    
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print('Job must be in list of approved jobs.')
+            
+    job=property(get_job,set_job)
+
+agent=Person('aaaaaon')
+print(agent.name)
+
+agent.name='ffffffffffffffffffffjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjfffffffffffffffffffffffffffffffffffffffffffffffffffff'
+print(agent.name)
+
+agent.job = 'Software developer'
+print(agent.job)
+agent.job='Backend developer'
+
+print(agent.job)
